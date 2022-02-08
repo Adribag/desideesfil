2adf4928a4c95891e867638d85872e11f195c662
Adri <adrien.baglin@gmail.com>
2022-02-08 10:15:16 +0100

    accueil + responsiv

--- desideesfil/base/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from base import views
-
-app_name ="base"
-urlpatterns = [
-    path('', views.index, name='index')
-]
--- desideesfil/desideesfil/urls.py
+++ desideesfil/desideesfil/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from base import views
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
